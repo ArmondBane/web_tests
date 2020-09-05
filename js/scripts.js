73a769cd09c4b621747d4c7817e921ce6c47a461
Aleksei <gohmsterwb@gmail.com>
2020-09-05 21:12:10 +0300

    Initialize

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#menu_button').on('click', function(){
+
+        alert('Hellow, it is menu!');
+
+    });
+
+    $('#head_title').hide(0).fadeIn(2000);
+
+});
