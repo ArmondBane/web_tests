1b9b20c92906453c5c6947e3c8e40667739b1e75
Aleksei <gohmsterwb@gmail.com>
2020-09-09 19:14:47 +0300

    add cookie plugin and cookie example

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 
+    $.cookie('name', 123);
+
     $('#menu_button').on('click', function(){
 
         alert('Hellow, it is menu!');
-
+        alert($.cookie('name'));
     });
 
     $('#head_title').hide(0).fadeIn(2000);
 
+    
 });
